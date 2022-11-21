b03e2f4f36c1b3c1c5236caf5e3cdbfd438d38bd
JM10PRO <108186529+JM10PRO@users.noreply.github.com>
2022-11-21 18:51:08 +0100

    Add files via upload

---
+++ tema1/addDomain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+grep -q -w "$1" /etc/hosts
+if [ $? -eq 0 ]
+then
+  echo "El dominio ya existe"
+else
+  echo "$2 $1" >> /etc/hosts
+echo "Se ha añadido el dominio $1 con IP $2 satisfactoriamente"
+fi
