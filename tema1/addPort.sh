a1cea297b31a3ecad8a060b0eeb7f8d5355dfce2
JM10PRO <108186529+JM10PRO@users.noreply.github.com>
2022-11-23 09:29:27 +0100

    Update addPort.sh

--- tema1/addPort.sh
+++ tema1/addPort.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-grep "Listen $1" /etc/apache2/ports.conf
+grep -q -w "Listen $1" /etc/apache2/ports.conf
 if [ $? -eq 0 ]
 then
-  echo "El puerto ya existe"
+  echo "El puerto $1 ya existe"
 else
   echo "Listen $1" >> /etc/apache2/ports.conf
+  echo Puerto $1 añadido
 fi
